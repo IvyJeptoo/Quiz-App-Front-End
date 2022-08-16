d2e8f271b47202a8b322892ea42e7f4fa0cc8329
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2022-08-16 09:57:07 +0300

    feat:create quiz display and style it

---
+++ quizapp/src/app/components/all-quiz/all-quiz.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-all-quiz',
+  templateUrl: './all-quiz.component.html',
+  styleUrls: ['./all-quiz.component.css']
+})
+export class AllQuizComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
