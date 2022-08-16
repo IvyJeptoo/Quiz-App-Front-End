2efdcc4501bb72f011c69c6e04c0a6341a1f52ed
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2022-08-16 09:54:41 +0300

    create home and quiz routes

--- quizapp/src/app/app-routing.module.ts
+++ quizapp/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { AllQuizComponent } from './components/all-quiz/all-quiz.component';
 
 const routes: Routes = [
-  {path: '', component: NavbarComponent},
+  {path: '', component: AllQuizComponent},
+  {path: 'allquiz', component: AllQuizComponent},
 
 ];
 
