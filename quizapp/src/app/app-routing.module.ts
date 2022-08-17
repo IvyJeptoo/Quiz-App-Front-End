ccf53f8e2f77880cdf57ce4ff75bd57d742f3fa8
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2022-08-18 00:31:38 +0300

    add routing and css styling

--- quizapp/src/app/app-routing.module.ts
+++ quizapp/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AllQuizComponent } from './components/all-quiz/all-quiz.component';
+import { CreateQuestionsComponent } from './components/create-questions/create-questions.component';
+import { CreatequizComponent } from './components/createquiz/createquiz.component';
+import { TakequizComponent } from './components/takequiz/takequiz.component';
 
 const routes: Routes = [
   {path: '', component: AllQuizComponent},
-  {path: 'allquiz', component: AllQuizComponent},
+  {path: 'takequiz', component: TakequizComponent},
+  {path: 'addquestions', component:CreateQuestionsComponent },
+  {path: 'createquiz', component: CreatequizComponent},
+
 
 ];
 
@@ -13,3 +19,8 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [
+  AllQuizComponent,
+  CreateQuestionsComponent,
+  CreateQuestionsComponent
+]
--- quizapp/src/app/app.module.ts
+++ quizapp/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { AllQuizComponent } from './components/all-quiz/all-quiz.component';
+import { CreatequizComponent } from './components/createquiz/createquiz.component';
+import { CreateQuestionsComponent } from './components/create-questions/create-questions.component';
+import { TakequizComponent } from './components/takequiz/takequiz.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AllQuizComponent,
+    CreatequizComponent,
+    CreateQuestionsComponent,
+    TakequizComponent
   ],
   imports: [
     BrowserModule,
