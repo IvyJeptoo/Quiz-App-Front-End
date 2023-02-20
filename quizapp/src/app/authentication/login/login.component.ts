86b4270e3c3947135f74d5b9b88ff00d2c840bec
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2023-02-20 19:46:03 +0300

    login authentication with modifications on html, css and ts for http requests

--- quizapp/src/app/authentication/login/login.component.ts
+++ quizapp/src/app/authentication/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,76 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+  submitted = false;
+  loginError = false;
+  showAlert = false;
 
-  constructor() { }
+  constructor(
+    private authService: AuthenticationService,
+    private formbuilder: FormBuilder,
+    private router: Router
+  ) { 
+    
+  }
 
-  ngOnInit(): void {
+
+  ngOnInit(): void {  
+    this.userLoginForm()   
+
+  }
+ 
+  userLoginForm(){
+    this.loginForm = this.formbuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+
+    })
+  }
+
+  onSubmit(){
+    console.log(this.loginForm.value);
+    this.submitted = true;
+    this.loginError = false;
+    
+    
+    if(this.loginForm.valid) {
+      const user = this.loginForm.value;
+      console.log('calling service');
+      
+      this.authService.userLogin(user).subscribe(
+        (response) => {
+          console.log(this.authService.isAuthenticated());
+          
+          this.router.navigate(['/welcome'])
+          const token = response.token;
+          localStorage.setItem('token', token);
+          console.log(token);
+          
+          // if(response && response.token){
+          //   this.authService.authenticated = true;
+          //   localStorage.setItem('token', response.token)
+          // }
+          // this.authService.setCurrentUser(response);
+          // const currentUser = this.authService.getCurrentUser()
+          // console.log('current user:', currentUser);
+          // console.log('authenticated', this.authService.authenticated);
+
+          
+
+        },
+        (error) => {
+          console.log('authenticated', this.authService.authenticated);
+
+          if( error.error.non_field_errors){
+            this.showAlert = true;
+          };
+          
+        }
+      );
+    }
+    
+
   }
 
 }
