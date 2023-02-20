78729373d8f9d822b6f5da530f6a02075f89b688
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2023-02-20 19:47:09 +0300

    signup authentication with modifications on html, css and ts for http requests

--- quizapp/src/app/authentication/signup/signup.component.ts
+++ quizapp/src/app/authentication/signup/signup.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { first, Observable } from 'rxjs';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +10,84 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  signUpForm!: FormGroup;
+  showAlert = false;
 
-  constructor() { }
+  constructor(
+    private formbuilder: FormBuilder,
+    private authService : AuthenticationService,
+    private router : Router
+    ) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { 
+    this.userSignUpForm()
+  }
+  userSignUpForm(){
+    this.signUpForm = this.formbuilder.group({
+      username: ['', [Validators.required, Validators.minLength(4)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', Validators.required],
+      role: ['', Validators.required]
+    }, {validator: this.passwordMatchValidator});
+  }  
+
+  passwordMatchValidator(formGroup: FormGroup){
+    const password = formGroup.get('password')?.value;
+    const confirmPassword = formGroup.get('confirmPassword')?.value;
+    if(password !== confirmPassword){
+      formGroup.get('confirmPassword')?.setErrors({mismatch: true})
+    } else {
+      formGroup.get('confirmPassword')?.setErrors(null)
+    }
+    
+  }
+
+  onSubmit(){
+    console.log('signupform valid:', this.signUpForm.valid);
+    
+    if(this.signUpForm.valid && this.signUpForm.dirty){
+      const user = this.signUpForm.value;
+      const role = this.signUpForm.get('role')?.value;
+      if(role === 'student'){
+        this.authService.signUpAsStudent(user)
+        .pipe(first())
+        .subscribe(
+          (response) => {
+            console.log('signup successful', response);
+            this.showAlert = true;
+            
+            this.router.navigate(['/authentication/login'])
+
+          },        
+          
+        
+        (error) => {
+          console.log('signup fail', error);
+          
+        }
+        );
+      } else if(role === 'teacher') {
+        this.authService.signUpAsTeacher(user)
+        .pipe(first())
+        .subscribe(
+          (response) => {
+            console.log('signup successful', response);  
+            this.showAlert = true;          
+            this.router.navigate(['/authentication/login'])
+            
+            
+
+          },
+          (error) =>{
+            console.log('signup fail', error);
+
+          }
+        )
+
+      }
+    }
+    
   }
 
 }
