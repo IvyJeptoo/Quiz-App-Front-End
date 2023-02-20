56e889d0d9446ea376513ef32ffccbf9814b5d0a
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2023-02-20 19:42:06 +0300

    create method for login and signup method to be used in the html

--- quizapp/src/app/app.component.ts
+++ quizapp/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from './services/authentication.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'quizapp';
+
+  constructor(
+    public authService: AuthenticationService,
+    private router: Router
+    ) {}
+
+    isLoginPage(){
+      return this.router.url === '/authentication/login'
+    }
+    isSignUpPage(){
+      return this.router.url === '/authentication/sign-up'
+    }
+    isWelcomePage(){
+      return this.router.url ==='/welcome'
+    }
 }
